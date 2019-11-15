d7e8a8f4f9d4fa24de075fe3acfcf3e9b86d13a8
Tnmy1991 <tonmoy.malik@hotmail.com>
2019-11-15 08:57:18 +0530

    First level sorting algorithm added

--- aws-on-demand-pricing.py
+++ aws-on-demand-pricing.py
@@ -1,6 +1,8 @@
 import requests
 import time
 import json
+from operator import itemgetter
+from itertools import groupby
 
 timestamp = str(round(time.time()))
 targetUrl = "https://a0.p.awsstatic.com/pricing/1.0/ec2/region/us-east-2/ondemand/linux/index.json?timestamp=" + timestamp
@@ -8,10 +10,22 @@ response = requests.get(targetUrl)
 print("Status code: " + str(response.status_code))
 data = json.loads(response.text)
 prices = data.get("prices")
-sortList = {}
+newList = []
 
 for price in prices:
 	attributes = price.get("attributes")
-	sortList.setdefault(attributes.get("aws:ec2:instanceFamily"), {}) = price
-	print(sortList)
-	exit()
+	attributes["id"] = price.get("id")
+	attributes["price"] = price.get("price").get("USD")
+	attributes["unit"] = price.get("unit")
+	newList.append(attributes)
+
+# First sort by required field
+# Groupby only finds groups that are collected consecutively
+newList.sort(key=itemgetter('aws:ec2:instanceFamily'))
+file = open("aws-on-demand-pricing.txt","a")
+
+# Now iterate through groups (here we will group by the year born)
+for instance_family, items in groupby(newList, key=itemgetter('aws:ec2:instanceFamily')):
+	file.write(instance_family)
+	for i in items:
+		file.write(str(i))
